b113deb3d76a652622303c9c405ac478c6b8a763
phfdonda <phfdonda@gmail.com>
2024-05-12 22:30:00 -0300

    Add Attributes button

--- src/components/sections/abilities/atributes/Attributes.jsx
+++ src/components/sections/abilities/atributes/Attributes.jsx
@@ -1,32 +1,53 @@
 // This subsection holds all limiter characteristics. These are not used in rolls, but instead are limiters to Talents. No Talent can be bigger than its Attribute limiter. 
 // For each 10 XP, one Attribute is gained, and one Advantage can be chosen.
 // ******************************************************************************
-
+import { useState } from "react";
 import Progress from "src/components/pieces/markers/Progress";
 import { $attributesData } from "src/state"
 import './styles/Attributes.scss'
 
 const Attributes = () => {
+    const [isOpen, setIsOpen] = useState(false)
     const attributesData = $attributesData.get()
-    console.log(attributesData)
 
     const renderAttributes = () => {
-        return attributesData.map((type) => {
-            return (
-                <section key={type.name} className='attributes-section'>
-                    <div className="type-title">
-                        <h1>{type.name}</h1>
+        const toggleAttributeSection = (e) => {
+            e.stopPropagation()
+            setIsOpen(!isOpen)
+        }
+        const renderAttributes = () => {
+            const attributes = []
+            attributesData.map((type) => {
+                attributes.push(
+                    <div key={type.name} className='attributes-section'>
+                        <div className="type-title">
+                            <h1>{type.name}</h1>
+                        </div>
+                        {
+                            type.attributes.map((attribute) => {
+                                return (
+                                    <Progress key={attribute.name} name={attribute.name} points={attribute.points} />
+                                )
+                            })
+                        }
                     </div>
-                    {
-                        type.attributes.map((attribute) => {
-                            return (
-                                <Progress key={attribute.name} name={attribute.name} points={attribute.points} />
-                            )
-                        })
-                    }
-                </section>
-            )
-        })
+                )
+            })
+            return attributes
+        }
+
+        return (
+            <section onClick={toggleAttributeSection} className="attributes">
+                <button type="button" className="attributes-section-title"><h1>Attributes</h1></button>
+                {isOpen &&
+                    <div>
+                        {renderAttributes()}
+                    </div>
+
+                }
+            </section >
+        )
+
     }
 
     return (
--- src/state/index.js
+++ src/state/index.js
@@ -4,25 +4,25 @@ export const $attributesData = atom([
     {
         name: "Corpo",
         attributes: [
-            { name: "Força", points: 2 },
-            { name: "Destreza", points: 3 },
-            { name: "Vigor", points: 3 },
+            { name: "Força", points: 1 },
+            { name: "Destreza", points: 1 },
+            { name: "Vigor", points: 1 },
         ],
     },
     {
         name: "Mente",
         attributes: [
-            { name: "Percepção", points: 2 },
-            { name: "Inteligência", points: 2 },
-            { name: "Raciocínio", points: 2 },
+            { name: "Percepção", points: 1 },
+            { name: "Inteligência", points: 1 },
+            { name: "Raciocínio", points: 1 },
         ],
     },
     {
         name: "Alma",
         attributes: [
-            { name: "Presença", points: 2 },
-            { name: "Vontade", points: 4 },
-            { name: "Sintonia", points: 2 },
+            { name: "Presença", points: 1 },
+            { name: "Vontade", points: 1 },
+            { name: "Sintonia", points: 1 },
         ],
     },
 ])
